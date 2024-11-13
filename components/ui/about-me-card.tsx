efefecfa944c347cc4091adfae8b649ed274a20e
Steven Gates <steven.gates@associa.us>
2024-11-13 08:20:08 -0600

    Revert "more clean up"
    This reverts commit 77abbd196db0b47243fdef68f498f93a3c3678d8.

--- components/ui/about-me-card.tsx
+++ components/ui/about-me-card.tsx
@@ -11,7 +11,6 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import Link from "next/link"
-import Image from "next/image"
 import { useState } from "react" // For dark mode toggle state
 
 interface AboutMeCardProps extends React.ComponentProps<typeof Card> {
@@ -55,7 +54,7 @@ export default function AboutMeCard({ className, ...props }: AboutMeCardProps) {
             <Card className={cn("w-3/4 max-w-[800px] p-6", className)} {...props}>
                 <CardHeader>
                     <div className="flex flex-col items-center space-y-4">
-                        <Image
+                        <img
                             src="/assets/me.jpg"
                             alt="Steven&apos;s profile picture"
                             className="w-24 h-24 rounded-full object-cover"
