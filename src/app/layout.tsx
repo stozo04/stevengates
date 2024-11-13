4d144e7a18b97eb8dbb9b1a486620b6b9352744b
Steven Gates <steven.gates@associa.us>
2024-11-13 15:31:04 -0600

    add google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from 'next/font/google';
 import Footer from "@/components/ui/footer";
+import GoogleAnalytics from "@/components/ui/googleAnalytics";
 
 const inter = Inter({subsets: ['latin']})
 
@@ -24,6 +25,7 @@ export default function RootLayout({
             {children}
           </div>
           <Footer />
+          <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
         </div>
       </body>
     </html>
